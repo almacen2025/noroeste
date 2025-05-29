f7351d77907db0b40761f0f2445f6064c717d76e
almacen2025 <german.surez@prosegur.com>
2025-05-29 15:49:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,137 +1,95 @@
-const productData = {
-  ajax: [
-    { code: "3402-000115", name: "Panel ajax" },
-    { code: "3404-000042", name: "Teclado Táctil ajax" },
-    { code: "3406-000060", name: "Magnético inalámbrico ajax" },
-    { code: "3412-000176", name: "Detector de movimiento Interior inalámbrico ajax" },
-    { code: "3416-000075", name: "Mando alarma 4 botones inalámbrico ajax" },
-    { code: "3412-000171", name: "PIRCAM interior inalámbrico ajax" },
-    { code: "3514-000097", name: "Tag de proximidad Ajax" },
-    { code: "3412-000172", name: "PIRCAM exterior inalámbrico ajax" },
-    { code: "3402-000132", name: "MÓDULO REPETIDOR DE SEÑALES INALÁMBRICO AJAX" },
-    { code: "3417-000187", name: "STREETSIREN AJAX" }
-  ],
-  climax: [
-    { code: "3402-000127", name: "CENTRAL HSGW-G8 - CLIMAX" },
-    { code: "3416-000089", name: "CONTROL REMOTO RC-15 - CLIMAX" },
-    { code: "3412-000159", name: "DETECTOR INALAMBRICO DOBLE TECNOLOGIA IRM-23B - CLIMAX" },
-    { code: "3419-000263", name: "MODULO CONVERTIDOR CABLEADO CLIMAX" },
-    { code: "3401-000549", name: "Panel Hibrido RF+16Z Cableadas CLIMAX" },
-    { code: "3404-000043", name: "Teclado Cableado LCD c/lector tags+ Sirena Climax" },
-    { code: "3401-000446", name: "Modulo expansor 8 zonas cableadas p/panel Hibrido Climax" },
-    { code: "2351-000533", name: "Climax fuente aliment panel Hibrido 3Amp V3 Part Number 8201230A50A6" },
-    { code: "3415-000043", name: "CLIMAX @ PANIC BUTTON CLIMAX REF. PB‐15‐P2" },
-    { code: "3406-000085", name: "TRANSMISOR MAGNETICO DC-23 - CLIMAX" },
-    { code: "3404-000048", name: "TECLADO INALAMBRICO C/LECTOR TAG KPT-32 - CLIMAX codigo nuevo" },
-    { code: "3412-000166", name: "VOLUMETRICO INTERIOR RADIO CLIMAX c/Antipet seleccionable REF. F919 Mhz IR¿32 codigo nuevo" },
-    { code: "3412-000165", name: "V.2 PIRCAM INDOOR PET IMMUNE REF. VST-892- V.2" }
-  ],
-  neo: [
-    { code: "3401-000470", name: "CENTRAL HS2064PCBARG - NEO" },
-    { code: "3419-000215", name: "COMUNICADOR DUAL TL2803GE-LAT - NEO" },
-    { code: "3412-000188", name: "DETECTOR INALAMBRICO DOBLE TECNOLOGIA PG9984P - NEO" },
-    { code: "3412-000187", name: "DETECTOR INALAMBRICO PG9904P - NEO" },
-    { code: "3412-000186", name: "DETECTOR PIRCAM PG9934P - NEO" },
-    { code: "3402-000028", name: "EXPANSOR DE ZONAS HSM2108 - NEO" },
-    { code: "2351-000114", name: "FUENTE DE ALIMENTACION HSM2300 - NEO" },
-    { code: "3006-000485", name: "GABINETE 585 - DSC - NEO" },
-    { code: "3419-000216", name: "RECEPTORA INALAMBRICA HSM2HOST9 - NEO" },
-    { code: "3500-000200", name: "REPETIDORA INALAMBRICA PG9920 - NEO" },
-    { code: "3514-000137", name: "TAG DE PROXIMIDAD - NEO" },
-    { code: "3403-000128", name: "TECLADO CABLEADO C/LECTOR TAG HS2LCDP - NEO" },
-    { code: "3404-000041", name: "TECLADO INALAMBRICO C/LECTOR TAG HS2LCDWFP9 - NEO" },
-    { code: "3416-000096", name: "CONTROL REMOTO PG9939 - NEO NUEVO" },
-    { code: "3405-000199", name: "MAGNETICO INALAMBRICO PG9945 - NEO NUEVO" }
-  ],
-  camaras: [
-  { code: "3352-000724", name: "CAMARA EXTERIOR - HIKVISION - CCTV" },
-  { code: "3354-000242", name: "CAMARA EXTERIOR - DAHUA" },
-  { code: "3354-000244", name: "CAMARA INFRARROJA SEMIEXTERIOR - DAHUA - CCTV" },
-  { code: "3352-000806", name: "CAMARA INFRARROJA SEMIEXTERIOR - HIKVISION - CCTV" },
-  { code: "3352-000697", name: "CAMARA GVMS CUBE IP INTERIOR - DAHUA IPC-K22 - CAM IP" },
-  { code: "3364-000073", name: "CAMARA GVMS DOMO IP EXTERIOR - DAHUA IPC-T26E - CAM IP" },
-  { code: "2351-000366", name: "CONECTOR ALIMENTACION PARA CAMARA - 5,5MM X 2,1MM - CCTV" },
-  { code: "2351-000447", name: "FUENTE 12V 10A 9C PARA CAMARA - CCTV" },
-  { code: "2351-000422", name: "FUENTE 12V 2A PARA CAMARA - CCTV (manager-v3)" },
-  { code: "3551-000047", name: "FUENTE 12V 30A 18C PARA CAMARA - CCTV" },
-  { code: "2351-000457", name: "FUENTE 12V 5A 4C PARA CAMARA - CCTV" },
-  { code: "3363-000023", name: "LENTE CAMARA FRONTAL - HIKVISION HV3816D-8MPIR" }
-],
-vista: [
-  { code: "3402-000117", name: "CENTRAL LYNX - HONEYWELL" },
-  { code: "3401-000465", name: "CENTRAL VISTA 48LA - HONEYWELL" },
-  { code: "3854-000490", name: "CONTROL REMOTO 5804/5834 - HONEYWELL" },
-  { code: "3402-000121", name: "CONTROL REMOTO ENCRYPTOR - P/CADDX-DSC-HONEYWELL" },
-  { code: "3854-000487", name: "DETECTOR INALAMBRICO 5800 - HONEYWELL" },
-  { code: "3406-000062", name: "MAGNETICO INALAMBRICO 5816 - HONEYWELL" },
-  { code: "3402-000124", name: "RECEPTOR ENCRYPTOR P/DSC-HONEYWELL" },
-  { code: "3419-000206", name: "RECEPTORA INALAMBRICA 5883H - HONEYWELL" },
-  { code: "3419-000205", name: "REPETIDORA INALAMBRICA 5800RP - HONEYWELL" },
-  { code: "3403-000157", name: "TECLADO 6148 - HONEYWELL" },
-  { code: "3403-000147", name: "TECLADO INALAMBRICO 5828V - HONEYWELL" },
-  { code: "3403-000149", name: "TECLADO RECEPTOR 6160RF - HONEYWELL" }
-],
-rsi: [
-  { code: "3402-000114", name: "CENTRAL WIP610/WIP630 - RSI" },
-  { code: "3416-000076", name: "CONTROL REMOTO RC601/KF641 - RSI" },
-  { code: "3412-000184", name: "DETECTOR EXTERIOR PIRCAM OMV-VX 601 - RSI" },
-  { code: "3412-000170", name: "DETECTOR PIR IMD601 - RSI" },
-  { code: "3409-000057", name: "DETECTOR PIRCAM IMV601 - RSI" },
-  { code: "2351-000108", name: "FUENTE WPS100 - RSI" },
-  { code: "2900-000042", name: "NUEVA SIM MOVISTAR M2M (CLIMAX, NEO, COMUNICADOR, RSI, GPS, GPRS, GSM, ajax)" },
-  { code: "2352-000055", name: "Batería de litio para Panel de alarmas RSI WIP630, RSI Mod. WBAT100" },
-  { code: "3419-000209", name: "PLACA WIFI WWB100 - RSI" },
-  { code: "3417-000142", name: "SIRENA EXTERIOR OSX601 - RSI" },
-  { code: "3417-000024", name: "SIRENA INTERIOR WIS100 - RSI" },
-  { code: "3514-000098", name: "TAG BR250 - RSI" },
-  { code: "3404-000040", name: "TECLADO WMB611 PROVIEW - RSI" },
-  { code: "3419-000210", name: "TRANSMISOR IUT601/ICT601 - RSI" }
-],
-  caddx: [
-  { code: "3401-000179", name: "CENTRAL NX-4 - CADDX" },
-  { code: "3850-000277", name: "CENTRAL NX-8 - CADDX" },
-  { code: "3402-000121", name: "CONTROL REMOTO ENCRYPTOR - P/CADDX-DSC-HONEYWELL" },
-  { code: "2351-000512", name: "EXPANSOR DE FUENTE NX-320 - CADDX" },
-  { code: "3401-000232", name: "EXPANSOR DE ZONAS NX-216 - CADDX" },
-  { code: "3403-000155", name: "TECLADO NX-148 - CADDX" },
-  { code: "3403-000154", name: "TECLADO NX-1508 - CADDX" }
-],
-caddx: [
-  { code: "3401-000179", name: "CENTRAL NX-4 - CADDX" },
-  { code: "3850-000277", name: "CENTRAL NX-8 - CADDX" },
-  { code: "3402-000121", name: "CONTROL REMOTO ENCRYPTOR - P/CADDX-DSC-HONEYWELL" },
-  { code: "2351-000512", name: "EXPANSOR DE FUENTE NX-320 - CADDX" },
-  { code: "3401-000232", name: "EXPANSOR DE ZONAS NX-216 - CADDX" },
-  { code: "3403-000155", name: "TECLADO NX-148 - CADDX" },
-  { code: "3403-000154", name: "TECLADO NX-1508 - CADDX" }
-],
-dvr: [
-  { code: "3361-000640", name: "DVR DE 16 CANALES - DAHUA - CCTV" },
-  { code: "3361-000603", name: "DVR DE 16 CANALES - HIKVISION - CCTV" },
-  { code: "3361-000602", name: "DVR DE 4 CANALES - HIKVISION - CCTV" },
-  { code: "3361-000641", name: "DVR DE 8 CANALES - DAHUA - CCTV" },
-  { code: "3361-000604", name: "DVR DE 8 CANALES - HIKVISION - CCTV" },
-  { code: "2351-000515", name: "FUENTE PARA DVR - CONECTOR 4 PINES - CCTV" }
-],
-optex: [
-  { code: "3412-000174", name: "DETECTOR INALAMBRICO VX-402R/VXI-R/VXS-RAM - OPTEX" },
-  { code: "3411-000281", name: "DETECTOR ANTIMASKING ES HX-40AM - OPTEX" },
-  { code: "3411-000131", name: "DETECTOR EXTERIOR CABLEADO VX-402/VXI-ST/VXS-AM - OPTEX" },
-  { code: "2403-000106", name: "DETECTOR LATERAL CABLEADO BX-80N/BXS-ST - OPTEX" },
-  { code: "3412-000173", name: "DETECTOR LATERAL INALAMBRICO BX-80NR/BXS-R - OPTEX" }
-]
-  
-};
-document.getElementById("searchInput").addEventListener("input", function () {
-  const query = this.value.toLowerCase().trim();
-  const list = document.getElementById("productList");
-  list.innerHTML = "";
+// Cargar SheetJS para exportar a Excel
+const script = document.createElement('script');
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js";
+document.head.appendChild(script);
+let inicio, intervalo;
+let datos = [];
+let cronometroActivo = false;
+let segundosTotales = 0;
 
-  if (productData[query]) {
-    productData[query].forEach(product => {
-      const li = document.createElement("li");
-      li.textContent = `${product.code} - ${product.name}`;
-      list.appendChild(li);
-    });
+// Cargar datos guardados al iniciar
+window.onload = function () {
+  const guardados = localStorage.getItem("registros");
+  if (guardados) {
+    datos = JSON.parse(guardados);
+    datos.forEach(registro => agregarFila(registro));
   }
-});
+};
+
+function iniciar() {
+  if (cronometroActivo) {
+    alert("El cronómetro ya está en marcha.");
+    return;
+  }
+
+  const nombre = document.getElementById("nombre").value.trim();
+  if (!nombre) {
+    alert("Por favor selecciona un nombre.");
+    return;
+  }
+
+  segundosTotales = 0;
+  document.getElementById("cronometro").textContent = "00:00:00";
+  inicio = new Date();
+  cronometroActivo = true;
+  intervalo = setInterval(actualizarCronometro, 1000);
+}
+
+function actualizarCronometro() {
+  segundosTotales++;
+
+  const horas = Math.floor(segundosTotales / 3600);
+  const minutos = Math.floor((segundosTotales % 3600) / 60);
+  const segundos = segundosTotales % 60;
+
+  const formato = 
+    String(horas).padStart(2, '0') + ':' +
+    String(minutos).padStart(2, '0') + ':' +
+    String(segundos).padStart(2, '0');
+
+  const cronometro = document.getElementById('cronometro');
+  cronometro.textContent = formato;
+
+  // Cambiar a rojo si pasa los 8 minutos
+  if (segundosTotales >= 480) {
+    cronometro.style.color = 'red';
+  } else {
+    cronometro.style.color = 'green';
+  }
+}
+
+function detener() {
+  if (!cronometroActivo) return;
+
+  clearInterval(intervalo);
+  cronometroActivo = false;
+
+  const select = document.getElementById("nombre");
+  const nombre = select.value;
+  const texto = select.options[select.selectedIndex].text;
+  const legajo = texto.match(/\((.*?)\)/)?.[1] || "";
+
+  const tiempo = document.getElementById("cronometro").textContent;
+  const fecha = new Date().toLocaleString();
+  const registro = { nombre, legajo, tiempo, fecha };
+
+  datos.push(registro);
+  localStorage.setItem("registros", JSON.stringify(datos));
+  agregarFila(registro);
+
+  document.getElementById("cronometro").textContent = "00:00:00";
+  select.selectedIndex = 0;
+}
+
+function agregarFila({ nombre, legajo, tiempo, fecha }) {
+  const fila = document.createElement("tr");
+  fila.innerHTML = `<td>${nombre}</td><td>${legajo}</td><td>${tiempo}</td><td>${fecha}</td>`;
+  document.querySelector("#tabla tbody").appendChild(fila);
+}
+
+function descargarExcel() {
+  const hoja = XLSX.utils.json_to_sheet(datos);
+  const libro = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(libro, hoja, "Registros");
+  XLSX.writeFile(libro, "registros.xlsx");
+}
